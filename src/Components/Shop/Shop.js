0a0015a0cf630fa3cfc453efee7260a855c9340d
Mahadeb Sen <mahadebsen237@gmail.com>
2022-03-26 10:01:10 +0530

    Shop added

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>Products section</h1>
+            <h2>Order Summery</h2>
+        </div>
+    );
+};
+
+export default Shop;
